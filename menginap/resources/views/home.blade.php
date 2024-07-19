d716d9f25b109c3863d8035ce2d6510f43053a80
V-Zlfkr <muhamad.zulfikar.official@gmail.com>
2024-07-19 21:39:34 +0800

    Squashed commit of the following:
    commit 5e525df0d71952e842a19bfc4c5b43979a0ddb66
    Author: V-Zlfkr <muhamad.zulfikar.official@gmail.com>
    Date:   Fri Jul 19 21:36:40 2024 +0800
    
        Commit 3 files
    
    commit b3d0d006b2517593a01d381a0a3f66c965de636f
    Author: V-Zlfkr <muhamad.zulfikar.official@gmail.com>
    Date:   Fri Jul 19 20:57:10 2024 +0800
    
        app css
    
    commit 10c2d9ba8bf76c6c365b872a6d00182d0fa709b5
    Author: V-Zlfkr <muhamad.zulfikar.official@gmail.com>
    Date:   Fri Jul 19 20:56:45 2024 +0800
    
        home css push 1

---
+++ menginap/resources/views/home.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/home.css">
+    <link rel="stylesheet" href="/css/app.css">
+    <title>Menginap</title>
+</head>
+<body>
+    <section>
+        <div class="headline">
+
+        </div>
+    </section>
+</body>
+</html>
