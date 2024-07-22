cf6d02539a994cc8f35808feb4849cfdb560a571
vzlfkr <160149232+vzlfkr@users.noreply.github.com>
2024-07-22 20:25:15 +0800

    Add Navbar Include (#1)

--- menginap/resources/views/home.blade.php
+++ menginap/resources/views/home.blade.php
@@ -5,10 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/css/home.css">
     <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="/css/navbar.css">
     <title>Menginap</title>
 </head>
 
 <body>
+    @include('components.navbar')
     <section>
         <div class="headline">
             <div class="img-div">
