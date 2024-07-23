669e00e2ada328e980e54e7589195ac6ba1acdf4
V-Zlfkr <muhamad.zulfikar.official@gmail.com>
2024-07-23 21:38:41 +0800

    Homepage FE - 23 July 2024
    Completed Navbar + Headline. Incomplete Hotel List and Footer

--- menginap/resources/views/components/navbar-no-reg.blade.php
+++ menginap/resources/views/components/navbar-no-reg.blade.php
@@ -1,13 +1,13 @@
 <header>
     <nav>
-        <div class="left-nav">
+        <div class="left-nav" onclick="window.location.href='/#'  ">
             <img src="/image/menginap-logo.png" alt="Menginap Logo Home">
             <h1>MENGINAP</h1>
         </div>
         <div class="center-nav">
-            <a href="/">Home</a>
-            <a href="#">Hotel List</a>
-            <a href="/#">About Us</a>
+            <a href="/" id="home">Home</a>
+            <a href="#" id="hotel-list">Hotel List</a>
+            <a href="#" id="about-us">About Us</a>
         </div>
         <div class="right-nav">
             <button>Sign In</button>
