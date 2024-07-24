1086bc1e370068dfd86e69e504561359f113aebb
vzlfkr <muhamad.zulfikar.official@gmail.com>
2024-07-24 10:34:48 +0800

    Squashed commit of the following:
    commit ccc61302feb74e8a068d16d79174802df5fdb270
    Author: vzlfkr <muhamad.zulfikar.official@gmail.com>
    Date:   Wed Jul 24 10:33:57 2024 +0800
    
        Delete File
    
        delete navbar file because it moved to component dir
    
    commit e7489be8109c95db00e21133683e86c7ca21dbcb
    Author: vzlfkr <muhamad.zulfikar.official@gmail.com>
    Date:   Wed Jul 24 09:50:34 2024 +0800
    
        Navbar After Log Fe
    
        All navbar FE after login
    
    commit aeb5bf54591d6964e6e262cd215334389b70e946
    Author: vzlfkr <160149232+vzlfkr@users.noreply.github.com>
    Date:   Tue Jul 23 21:43:24 2024 +0800
    
        Homepage fe to navbar-non-regist-fe (#2)
    
        * Add Navbar Include
    
        * ignore composer lock
    
        * 90% navbar-non-reg complete
    
        Almost done with the navbar only 10% left
    
        * Homepage FE - 23 July 2024
    
        Completed Navbar + Headline. Incomplete Hotel List and Footer
    
    commit ad7d4c8cdcd81ec243b3248de003b60040cdaea3
    Author: V-Zlfkr <muhamad.zulfikar.official@gmail.com>
    Date:   Mon Jul 22 19:53:58 2024 +0800
    
        Zul Navbar

---
+++ menginap/resources/views/components/navbar-log.blade.php
@@ -0,0 +1,17 @@
+<header>
+    <nav>
+        <div class="left-nav" onclick="window.location.href='/#'  ">
+            <img src="/image/menginap-logo.png" alt="Menginap Logo Home">
+            <h1>MENGINAP</h1>
+        </div>
+        <div class="center-nav">
+            <a href="/" id="home">Home</a>
+            <a href="#" id="hotel-list">Hotel List</a>
+            <a href="#" id="about-us">About Us</a>
+        </div>
+        <div class="right-nav">
+            <i class="fa-solid fa-user fa-2x"></i>
+            <h1>Profile</h1>
+        </div>
+    </nav>
+</header>
--- menginap/resources/views/home.blade.php
+++ menginap/resources/views/home.blade.php
@@ -5,10 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/css/home.css">
     <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="/css/navbar-no-reg.css">
+    <link rel="stylesheet" href="/css/navbar-log.css">
+    <script src="https://kit.fontawesome.com/bf51598d13.js" crossorigin="anonymous"></script>
     <title>Menginap</title>
 </head>
 
 <body>
+    @include('components.navbar-log')
     <section>
         <div class="headline">
             <div class="img-div">
