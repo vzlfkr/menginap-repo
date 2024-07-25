68c975c2bf4c7236d4828eed63d64bca137bd5c9
vzlfkr <muhamad.zulfikar.official@gmail.com>
2024-07-25 09:15:50 +0800

    User Page 90%
    The user page is still at 90% progress (without footer)

---
+++ menginap/resources/views/user-page.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="/css/navbar-log.css">
+    <link rel="stylesheet" href="/css/user-page.css">
+    <script src="https://kit.fontawesome.com/bf51598d13.js" crossorigin="anonymous"></script>
+    <title>Document</title>
+</head>
+
+<body>
+    @include('components.navbar-log')
+    <section>
+        <div class="div-1">
+            <div class="div-2">
+                <div class="div-4">
+                    <p><span>Hi, </span>Name</p>
+                </div>
+                <div class="div-5">
+                    <h3>Full Name</h3>
+                    <p>Name</p>
+                </div>
+                <div class="div-6">
+                    <h3>E-mail</h3>
+                    <p>test@gmail.com</p>
+                </div>
+                <div class="div-7">
+                    <h3>Contact Number</h3>
+                    <p>123456789</p>
+                </div>
+            </div>
+            <div class="div-3">
+                <img src="/image/img-placeholder-square.jpg" alt="Profile Placeholder">
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
