3735af536464ab154655b94a52b636cf47c49aaa
vzlfkr <muhamad.zulfikar.official@gmail.com>
2024-07-26 20:45:53 +0800

    Fokoter Uncompleted
    Still not completed yet

--- menginap/resources/views/components/footer.blade.php
+++ menginap/resources/views/components/footer.blade.php
@@ -1,5 +1,13 @@
 <footer>
-    <div>
-
+    <div class="div-main">
+        <div class="footer-div-1 footer-div">
+            <img src="/image/menginap-logo.png" alt="Menginap Logo Home">
+            <h1>MENGINAP</h1>
+        </div>
+        <div class="footer-div-2 footer-div">
+            <div class="footer-div-3 footer-div"></div>
+            <div class="footer-div-4 footer-div"></div>
+            <div class="footer-div-5 footer-div"></div>
+        </div>
     </div>
 </footer>
--- menginap/resources/views/home.blade.php
+++ menginap/resources/views/home.blade.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="/css/app.css">
     <link rel="stylesheet" href="/css/navbar-no-reg.css">
     <link rel="stylesheet" href="/css/navbar-log.css">
+    <link rel="stylesheet" href="/css/footer.css">
     <script src="https://kit.fontawesome.com/bf51598d13.js" crossorigin="anonymous"></script>
     <title>Menginap</title>
 </head>
