f02421bd0e5473eba5ce153c8e138ffe660cd33a
DewaGedeEkaBudhiarta <dodehebat86@gmail.com>
2024-07-26 20:31:03 +0800

    updated register blade

--- menginap/resources/views/user/index.blade.php
+++ menginap/resources/views/user/index.blade.php
@@ -25,23 +25,43 @@ style="background-image: url('https://mdbcdn.b-cdn.net/img/Photos/new-templates/
             <form action="{{ route('user.store') }}" method="POST">
               @csrf
               <div data-mdb-input-init class="form-outline mb-4">
-                <input type="text" name="name" id="name" class="form-control form-control-lg" />
+                <input type="text" name="name" id="name" class="form-control form-control-lg @error('name') is-invalid @enderror" value="{{ old('name') }}" />
                 <label class="form-label" for="name">Full name</label>
+                @error('name')
+                  <div class="invalid-feedback">
+                      {{ $message }}
+                  </div>
+                @enderror
               </div>
 
               <div data-mdb-input-init class="form-outline mb-4">
-                <input type="email" name="email" id="email" class="form-control form-control-lg" />
+                <input type="email" name="email" id="email" class="form-control form-control-lg @error('email') is-invalid @enderror" value="{{ old('email') }}" />
                 <label class="form-label" for="email">Your Email</label>
+                @error('email')
+                  <div class="invalid-feedback">
+                      {{ $message }}
+                  </div>
+                @enderror
               </div>
 
               <div data-mdb-input-init class="form-outline mb-4">
-                <input type="password" name="password" id="password" class="form-control form-control-lg" />
+                <input type="password" name="password" id="password" class="form-control form-control-lg @error('password') is-invalid @enderror" value="{{ old('password') }}" />
                 <label class="form-label" for="password">Password</label>
+                @error('password')
+                  <div class="invalid-feedback">
+                      {{ $message }}
+                  </div>
+                @enderror
               </div>
 
               <div data-mdb-input-init class="form-outline mb-4">
-                <input type="int" name="noHp" id="noHp" class="form-control form-control-lg" />
+                <input type="int" name="noHp" id="noHp" class="form-control form-control-lg @error('noHp') is-invalid @enderror" value="{{ old('noHp') }}" />
                 <label class="form-label" for="noHp">Phone Number</label>
+                @error('noHp')
+                  <div class="invalid-feedback">
+                      {{ $message }}
+                  </div>
+                @enderror
               </div>
 
               <div class="d-flex justify-content-center">
@@ -63,6 +83,5 @@ style="background-image: url('https://mdbcdn.b-cdn.net/img/Photos/new-templates/
 </section>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </body>
 </html>
--- menginap/resources/views/user/login.blade.php
+++ menginap/resources/views/user/login.blade.php
@@ -58,6 +58,5 @@ style="background-image: url('https://mdbcdn.b-cdn.net/img/Photos/new-templates/
 </section>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </body>
 </html>
