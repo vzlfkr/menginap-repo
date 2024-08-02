a1f8fab8db60844401e9e32f2b35a452c3084b6b
vzlfkr <muhamad.zulfikar.official@gmail.com>
2024-08-02 09:46:06 +0800

    About Component Template Created
    About Component Template Created

---
+++ menginap/resources/views/about.blade.php
@@ -0,0 +1,24 @@
+<div class="about">
+    <div class="div-1">
+        <h3>
+            OUR STORY
+        </h3>
+    </div>
+    <div class="div-2">
+        <div class="div-3">
+            <div class="div-5">
+                <h1>
+                    LOREM IPSUM
+                </h1>
+            </div>
+            <div class="div-6">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ultricies orci in ultrices maximus. Duis sodales lacus eu elit scelerisque tempor. Praesent placerat nulla et auctor molestie. Aenean sagittis molestie interdum. Nullam nec nisl sapien. Quisque cursus, est a pellentesque viverra, enim ipsum imperdiet tortor, id placerat nisl arcu nec lacus. Cras ex magna, vulputate non volutpat at, porta eget nisl. Maecenas facilisis odio eu ornare elementum. Morbi vitae elementum nisl. Morbi pellentesque, purus id ultricies tempus, velit lectus viverra purus, ut eleifend lacus felis sed ante.
+                </p>
+            </div>
+        </div>
+        <div class="div-4">
+            <img src="/image/skyscrapers.jpg" alt="Skyscrapers">
+        </div>
+    </div>
+</div>
