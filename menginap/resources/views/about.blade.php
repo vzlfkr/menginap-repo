4df5c58495b52a9e7a3843422241b92492315057
V-Zlfkr <muhamad.zulfikar.official@gmail.com>
2024-08-11 14:10:37 +0800

    Add About Us element Redirect
    Add About Us element Redirect

--- menginap/resources/views/about.blade.php
+++ menginap/resources/views/about.blade.php
@@ -1,3 +1,4 @@
+<div class="about-anchor" id="about-page"></div>
 <div class="about">
     <div class="div-1-abt">
         <h3>
@@ -13,7 +14,13 @@
             </div>
             <div class="div-6-abt">
                 <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ultricies orci in ultrices maximus. Duis sodales lacus eu elit scelerisque tempor. Praesent placerat nulla et auctor molestie. Aenean sagittis molestie interdum. Nullam nec nisl sapien. Quisque cursus, est a pellentesque viverra, enim ipsum imperdiet tortor, id placerat nisl arcu nec lacus. Cras ex magna, vulputate non volutpat at, porta eget nisl. Maecenas facilisis odio eu ornare elementum. Morbi vitae elementum nisl. Morbi pellentesque, purus id ultricies tempus, velit lectus viverra purus, ut eleifend lacus felis sed ante.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ultricies orci in ultrices maximus.
+                    Duis sodales lacus eu elit scelerisque tempor. Praesent placerat nulla et auctor molestie. Aenean
+                    sagittis molestie interdum. Nullam nec nisl sapien. Quisque cursus, est a pellentesque viverra, enim
+                    ipsum imperdiet tortor, id placerat nisl arcu nec lacus. Cras ex magna, vulputate non volutpat at,
+                    porta eget nisl. Maecenas facilisis odio eu ornare elementum. Morbi vitae elementum nisl. Morbi
+                    pellentesque, purus id ultricies tempus, velit lectus viverra purus, ut eleifend lacus felis sed
+                    ante.
                 </p>
             </div>
         </div>
--- menginap/resources/views/components/navbar.blade.php
+++ menginap/resources/views/components/navbar.blade.php
@@ -7,7 +7,7 @@
         <div class="center-nav">
             <a href="/" id="home">Home</a>
             <a href="/list" id="hotel-list">Hotel List</a>
-            <a href="#" id="about-us">About Us</a>
+            <a href="/#about-page" id="about-us">About Us</a>
         </div>
         <div class="right-nav-case">
             @auth
